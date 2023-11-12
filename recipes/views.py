912eb6f84b85b6efec17faab6dd336e64fd4505d
Pedro Henrique <pedrohs21@outlook.com>
2023-11-12 17:28:11 -0300

    Header finalizado

--- recipes/views.py
+++ recipes/views.py
@@ -3,7 +3,4 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home(request):
-	return render(request, 'recipes/home.html')
-
-def contato(request):
-	return HttpResponse('Contato')
+	return render(request, 'recipes/pages/home.html')
