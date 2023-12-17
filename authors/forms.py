50ba11dc1c20898e1a2c9954ffdbd87ff04a35c7
Pedro Henrique <pedrohs21@outlook.com>
2023-12-17 17:38:26 -0300

    Criação da função `add_attr` para adicionar atributos em campos do formulário de registro

--- authors/forms.py
+++ authors/forms.py
@@ -1,7 +1,32 @@
 from django import forms
 from django.contrib.auth.models import User
 
-class RegisterForm (forms.ModelForm):
+def add_attr(field, attr_name, attr_new_val):
+    existing = field.widget.attrs.get(attr_name, '')
+    field.widget.attrs[attr_name] = f'{existing} {attr_new_val}'.strip()
+
+def add_placeholder(field, placeholder_val):
+    add_attr(field, 'placeholder', placeholder_val)
+
+class RegisterForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        add_placeholder(self.fields['username'], 'Your username')
+        add_placeholder(self.fields['email'], 'Your email')
+
+    password2 = forms.CharField(
+        required=True,
+        widget=forms.PasswordInput(attrs={
+            'placeholder': 'Repeat your password'
+        }),
+        error_messages={
+            'required': 'Password must not be empty'
+        },
+        help_text=(
+            'Password must be at least 8 characters.'
+        )
+    )
+
     class Meta():
         model = User
         fields = [
