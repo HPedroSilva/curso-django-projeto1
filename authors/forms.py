f1443afd9ba5c2d538e8c2195fe7f501333607bd
Pedro Henrique <pedrohs21@outlook.com>
2023-12-17 18:26:47 -0300

    Inserindo funções de validação em campos do formulário de registro

--- authors/forms.py
+++ authors/forms.py
@@ -1,5 +1,8 @@
+import re
+
 from django import forms
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
 def add_attr(field, attr_name, attr_new_val):
     existing = field.widget.attrs.get(attr_name, '')
@@ -8,12 +11,34 @@ def add_attr(field, attr_name, attr_new_val):
 def add_placeholder(field, placeholder_val):
     add_attr(field, 'placeholder', placeholder_val)
 
+def strong_password(password):
+    regex = re.compile(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8,}$')
+
+    if not regex.match(password):
+        raise ValidationError(
+            'Password too weak, please improve the difficulty of your password',
+            code='invalid',
+        )
+
 class RegisterForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         add_placeholder(self.fields['username'], 'Your username')
         add_placeholder(self.fields['email'], 'Your email')
 
+    password = forms.CharField(
+        required=True,
+        widget=forms.PasswordInput(attrs={
+            'placeholder': 'Your password'
+        }),
+        error_messages={
+            'required': 'Password must not be empty'
+        },
+        help_text=(
+            'Password must be at least 8 characters.'
+        ),
+        validators=[strong_password]
+    )
     password2 = forms.CharField(
         required=True,
         widget=forms.PasswordInput(attrs={
@@ -60,3 +85,15 @@ class RegisterForm(forms.ModelForm):
             }),
             'password': forms.PasswordInput(),
         }
+    
+    def clean(self):
+        cleaned_data = super().clean()
+
+        password = cleaned_data.get('password')
+        password2 = cleaned_data.get('password2')
+
+        if password != password2:
+            raise ValidationError({
+                'password': 'Passwords must be equal',
+                'password2': 'Passwords must be equal',
+            })
