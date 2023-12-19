dbf258367dae9094e32225bc3dba0ad6139b4399
Pedro Henrique <pedrohs21@outlook.com>
2023-12-19 17:46:06 -0300

    Criação da página `dashboard`

--- authors/views.py
+++ authors/views.py
@@ -54,7 +54,7 @@ def login_create(request):
             username=form.cleaned_data.get('username', ''),
             password=form.cleaned_data.get('password', ''),
         )
-        
+
         if authenticated_user is not None:
             messages.success(request, 'You are logged in.')
             login(request, authenticated_user)
@@ -63,7 +63,7 @@ def login_create(request):
     else:
         messages.error(request, 'Invalid username or password')
 
-    return redirect('authors:login')
+    return redirect(reverse('authors:dashboard'))
 
 @login_required(login_url='authors:login', redirect_field_name='next')
 def logout_view(request):
@@ -76,3 +76,7 @@ def logout_view(request):
     logout(request)
 
     return redirect(reverse('authors:login'))
+
+@login_required(login_url='authors:login', redirect_field_name='next')
+def dashboard(request):
+    return render(request, 'authors/pages/dashboard.html')
