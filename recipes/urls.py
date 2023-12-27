70adf037c0740b7cee8685cd532e6d6f42ad01ca
Pedro Henrique <pedrohs21@outlook.com>
2023-12-27 15:19:36 -0300

    Criação de views com `JSONResponse`

--- recipes/urls.py
+++ recipes/urls.py
@@ -13,4 +13,12 @@ urlpatterns = [
         name="category",
     ),
     path("recipes/<int:pk>/", views.RecipeDetailView.as_view(), name="recipe"),
+    path(
+        "recipes/api/v1/", views.RecipeListViewHomeApi.as_view(), name="recipes_api_v1"
+    ),
+    path(
+        "recipes/api/v1/<int:pk>/",
+        views.RecipeDetailAPI.as_view(),
+        name="recipe_api_v1_detail",
+    ),
 ]
