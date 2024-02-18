4c831f62e3606c6f3e5e6df8a142853522a8d58f
Pedro Henrique <pedrohs21@outlook.com>
2024-02-18 15:19:10 -0300

    Configuração do projeto para o uso de autenticação com tokens JWT

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path, include
 
 from rest_framework.routers import SimpleRouter
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
 
 from recipes import views
 
@@ -41,5 +46,10 @@ urlpatterns = [
     path(
         'recipes/api/v2/tag/<int:pk>', views.tag_api_detail, name='recipes_api_v2_tag'
     ),
+    path('recipes/api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path(
+        'recipes/api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'
+    ),
+    path('recipes/api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     path('', include(recipe_api_v2_router.urls)),
 ]
