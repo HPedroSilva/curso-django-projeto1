ddf74c2337f70912b3afd42c0e4aa65274f5eada
Pedro Henrique <pedrohs21@outlook.com>
2024-02-25 21:49:04 -0300

    Implementa o uso de permissões de acesso na API

--- recipes/views/api.py
+++ recipes/views/api.py
@@ -3,11 +3,13 @@ from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.viewsets import ModelViewSet
 
 from recipes.models import Recipe
 from tag.models import Tag
 from recipes.serializers import RecipeSerializer, TagSerializer
+from recipes.permissions import IsOwner
 
 
 class RecipeAPIv2Pagination(PageNumberPagination):
@@ -18,6 +20,36 @@ class RecipeAPIv2ViewSet(ModelViewSet):
     queryset = Recipe.objects.get_published()
     serializer_class = RecipeSerializer
     pagination_class = RecipeAPIv2Pagination
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+
+        category_id = self.request.query_params.get('category_id', '')
+
+        if category_id != '' and category_id.isnumeric():
+            qs = qs.filter(category_id=category_id)
+
+        return qs
+
+    def get_object(self):
+        pk = self.kwargs.get('pk', '')
+
+        obj = get_object_or_404(
+            self.get_queryset(),
+            pk=pk,
+        )
+
+        self.check_object_permissions(self.request, obj)
+
+        return obj
+
+    def get_permissions(self):
+        if self.request.method in ['PATCH', 'DELETE']:
+            return [
+                IsOwner(),
+            ]
+        return super().get_permissions()
 
 
 @api_view()
