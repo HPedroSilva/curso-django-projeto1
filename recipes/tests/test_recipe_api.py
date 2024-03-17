a6e368f0a39649c82c72ada7c9b47b9c1a96f7ca
Pedro Henrique <pedrohs21@outlook.com>
2024-03-17 16:49:03 -0300

    Implementação de testes na api de recipes

--- recipes/tests/test_recipe_api.py
+++ recipes/tests/test_recipe_api.py
@@ -5,9 +5,14 @@ from rest_framework import test
 
 
 class RecipeAPIv2Test(test.APITestCase, RecipeMixin):
-    def test_recipe_api_list_returns_status_code_200(self):
-        api_url = reverse('recipes:recipes-api-list')
+    def get_recipe_api_list(self, reverse_result=None):
+
+        api_url = reverse_result or reverse('recipes:recipes-api-list')
         response = self.client.get(api_url)
+        return response
+
+    def test_recipe_api_list_returns_status_code_200(self):
+        response = self.get_recipe_api_list()
         self.assertEqual(response.status_code, 200)
 
     @patch('recipes.views.api.RecipeAPIv2Pagination.page_size', new=7)
@@ -19,3 +24,34 @@ class RecipeAPIv2Test(test.APITestCase, RecipeMixin):
         qtd_of_loaded_recipes = len(response.data.get('results'))
 
         self.assertEqual(wanted_number_of_recipes, qtd_of_loaded_recipes)
+
+    def test_recipe_api_do_not_show_not_published_recipes(self):
+        recipes = self.make_recipe_in_batch(qtd=2)
+        recipe_not_published = recipes[0]
+        recipe_not_published.is_published = False
+        recipe_not_published.save()
+
+        response = self.get_recipe_api_list()
+
+        self.assertEqual(len(response.data.get('results')), 1)
+
+    @patch('recipes.views.api.RecipeAPIv2Pagination.page_size', new=10)
+    def test_recipe_api_lads_recipes_by_category_id(self):
+        category_wanted = self.make_category(name='wanted_category')
+        category_not_wanted = self.make_category(name='not_wanted_category')
+
+        recipes = self.make_recipe_in_batch(qtd=10)
+
+        for recipe in recipes:
+            recipe.category = category_wanted
+            recipe.save()
+
+        recipes[0].category = category_not_wanted
+        recipes[0].save()
+
+        api_url = (
+            reverse('recipes:recipes-api-list') + f'?category_id={category_wanted.id}'
+        )
+        response = self.get_recipe_api_list(reverse_result=api_url)
+
+        self.assertEqual(len(response.data.get('results')), 9)
